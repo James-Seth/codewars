4a0aa1d59bcc3bb8830846561c474f05b95ffee6
James-Seth <sethjames714@gmail.com>
2023-06-22 11:46:33 -0400

    codewars challenges

---
+++ 8kyu/hhh.js
@@ -0,0 +1,20 @@
+// Given a non-empty array of integers, return the result of multiplying the values together in order. 
+//Example:
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+//My Solution:
+function grow(x){
+    return x.reduce((a, b) => a * b)
+   }
+
+//Other Solutions: 
+const grow = x => {
+    let res = 1;
+    for (let i = 0; i < x.length; i++) {
+      res *= x[i];
+    }
+    return res;
+  };
+
+//Another one:
+const grow = (nums) => nums.reduce((product, num) => product * num, 1);
