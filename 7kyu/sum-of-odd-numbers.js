6945368c778ca6a4ee80d22a5f031af4af2464ff
James-Seth <sethjames714@gmail.com>
2023-07-20 09:14:41 -0400

    codewar challenges

---
+++ 7kyu/sum-of-odd-numbers.js
@@ -0,0 +1,28 @@
+//Description
+// Given the triangle of consecutive odd numbers:
+
+//              1
+//           3     5
+//        7     9    11
+//    13    15    17    19
+// 21    23    25    27    29
+// ...
+
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+// 1 -->  1
+// 2 --> 3 + 5 = 8
+
+//My Solution:
+function rowSumOddNumbers(n) {
+    return Math.pow(n, 3);
+  }
+
+//Other Solutions:
+let rowSumOddNumbers = n => n ** 3;
+
+//Another One:
+//Double asterisk able to be used in certain enviroments.
+function rowSumOddNumbers(n) {
+	return n**3
+}
