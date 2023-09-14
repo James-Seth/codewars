0a59984735d0851f62b60e4d015dfe44b39a5e02
James-Seth <133886371+James-Seth@users.noreply.github.com>
2023-09-14 08:57:55 -0400

    codewar challenges

---
+++ 8kyu/make-uppercase.js
@@ -0,0 +1,10 @@
+//Description:
+//Write a function which converts the input string to uppercase.
+
+//My Solution:
+function makeUpperCase(str) {
+    return str.toUpperCase();
+}
+
+//Other solutions:
+const makeUpperCase = str => str.toUpperCase();
