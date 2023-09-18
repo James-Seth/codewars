be54d4c58b4f00f4d715ed29696b8b9ec1bd7549
James-Seth <133886371+James-Seth@users.noreply.github.com>
2023-09-18 08:08:21 -0400

    codewar challenges

---
+++ 8kyu/square-n-sum.js
@@ -0,0 +1,18 @@
+//Describtion:
+// Complete the square sum function so that it squares each number passed 
+// into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 12+22+22=91^2 
+// + 2^2 + 2^2 = 912+22+22=9.
+
+//My Solution:
+function squareSum(numbers){
+    return numbers.reduce(function(sum, n){
+      return (n * n) + sum;
+    }, 0)
+  }
+
+//Other Solutions:
+function squareSum(numbers){
+    return numbers.reduce((sum,num) => sum + (num * num), 0);
+  }
