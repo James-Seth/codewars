e80e91c5a7cb949e97fc4cb639500ad189695775
James-Seth <sethjames714@gmail.com>
2023-12-31 20:56:04 -0500

    codewar challenges

---
+++ 8kyu/convert-a-boolean-to-a-string.js
@@ -0,0 +1,14 @@
+//Describtion:
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+//My Solution:
+function booleanToString(b){
+    return b.toString()
+  }
+
+//Other solutinos
+function booleanToString(b){
+    return b ? 'true' : 'false';
+  }
