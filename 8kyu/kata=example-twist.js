e80c27ee60061baccb485c6cfabc5bb5a4f30395
Seth James <sethjames714@gmail.com>
2024-07-11 10:41:11 -0400

    codewar challenges

---
+++ 8kyu/kata=example-twist.js
@@ -0,0 +1,11 @@
+//Describtion:
+// This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+// Add the value "codewars" to the array websites 1,000 times.
+
+//My Solution:
+let websites = new Array(1000).fill("codewars");
+
+//Other solutions:
+var websites = [];
+while (websites.length < 1000) websites.push("codewars")
