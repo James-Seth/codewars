a0d490b82f87c1e3387b5d044de83ab4f727ef88
Seth James <sethjames714@gmail.com>
2024-09-06 07:15:42 -0400

    codewar challenges

---
+++ 8kyu/get-ascii-value-of-character.js
@@ -0,0 +1,12 @@
+//Describtion:
+// Get ASCII value of a character.
+
+// For the ASCII table you can refer to http://www.asciitable.com/
+
+//My Solution:
+function getASCII(c){
+    return c.charCodeAt()
+  }
+
+//Other solutoins:
+const getASCII = c => c.charCodeAt()
