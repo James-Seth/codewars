a77d3c2cd2aa17a957afabaab5de13888b3c5fee
Seth James <sethjames714@gmail.com>
2025-02-10 11:33:30 -0500

    codewar challenges

---
+++ Python/8kyu/enumerable-magic-25-take-the-first-n-elements.py
@@ -0,0 +1,14 @@
+#Descrbition:
+# Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+# If you need help, here's a reference:
+
+# https://docs.python.org/3/library/stdtypes.html#sequence-types-list-tuple-range
+
+#Solution:
+def take(arr,n):
+    return arr[slice(n)]
+
+#Other solutions:
+def take(arr,n):
+    return arr[:n]
