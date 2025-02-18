351e375d766e1442aaa2c8cd2d153d162cec38e7
Seth James <sethjames714@gmail.com>
2025-02-18 08:05:25 -0500

    codewar challenges

---
+++ Python/8kyu/opposite-number.py
@@ -0,0 +1,16 @@
+#Describtion:
+# Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+# Examples:
+
+# 1: -1
+# 14: -14
+# -34: 34
+
+#Solution:
+def opposite(number):
+    return -number
+
+#Other solutions:
+def opposite(number):
+  return number * -1
