4f1c1740a888c156eafc764e4869637ded430fce
Seth James <sethjames714@gmail.com>
2025-03-10 08:53:06 -0400

    codewar challenges

---
+++ Python/8kyu/returning-strings.py
@@ -0,0 +1,12 @@
+#Describtion:
+# Create a function that accepts a parameter representing a name and returns the message: "Hello, <name> how are you doing today?".
+
+# [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+#Solution:
+def greet(name):
+    return f'Hello, {name} how are you doing today?'
+
+#Other solutions:
+def greet(name):
+    return "Hello, " + name + " how are you doing today?"
