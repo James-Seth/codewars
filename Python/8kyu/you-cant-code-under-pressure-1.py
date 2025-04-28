bc5231d62a421864d87259f6126f909be06bce3b
Seth James <sethjames714@gmail.com>
2025-04-28 17:04:33 -0400

    codewar challenges

---
+++ Python/8kyu/you-cant-code-under-pressure-1.py
@@ -0,0 +1,14 @@
+#Describion:
+# Code as fast as you can! You need to double the integer and return it.
+
+#Solution:
+def doubleInteger(i):
+    return i * 2
+
+#Other solutions:
+def doubleInteger(i):
+    return i + i
+
+#Another:
+def doubleInteger(i):
+    return i << 1
