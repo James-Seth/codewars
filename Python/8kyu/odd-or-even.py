8e87a54a0108447dda29584a09cb82e0bd36b23c
Seth James <sethjames714@gmail.com>
2025-05-08 09:26:25 -0400

    codewar challenges

---
+++ Python/8kyu/odd-or-even.py
@@ -0,0 +1,28 @@
+#Describtion:
+# Task:
+
+# Given a list of integers, determine whether the sum of its elements is odd or even.
+
+# Give your answer as a string matching "odd" or "even".
+
+# If the input array is empty consider it as: [0] (array with a zero).
+# Examples:
+
+# Input: [0]
+# Output: "even"
+
+# Input: [0, 1, 4]
+# Output: "odd"
+
+# Input: [0, -1, -5]
+# Output: "even"
+
+# Have fun!
+
+#Solution:
+def oddOrEven(arr):
+    return 'even' if sum(arr) % 2 == 0 else 'odd'
+
+#Other solutions:
+def oddOrEven(arr):
+    return ('even', 'odd')[sum(arr) % 2]
