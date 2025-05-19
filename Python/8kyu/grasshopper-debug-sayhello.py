1ee5a7c4546310a93c60e69c0eff178107c86c63
Seth James <sethjames714@gmail.com>
2025-05-18 20:49:41 -0400

    codewar challenges

---
+++ Python/8kyu/grasshopper-debug-sayhello.py
@@ -0,0 +1,17 @@
+#Describtion:
+# Debugging sayHello function
+
+# The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+# Example output:
+
+# Hello, Mr. Spock
+
+#Solution:
+def say_hello(name):
+    return f"Hello, {name}"
+
+#Other solutions:
+def say_hello(name):
+    return f"Hello, {name}"
+
