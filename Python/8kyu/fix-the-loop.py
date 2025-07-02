5abacac38c700f4939fcd6e971b99ade3ea0db40
Seth James <sethjames714@gmail.com>
2025-07-02 07:32:43 -0400

    codewar challlenges

---
+++ Python/8kyu/fix-the-loop.py
@@ -0,0 +1,10 @@
+#Describtion:
+
+#Soltion:
+def list_animals(animals):
+    list = ''
+    for i in range(len(animals)):
+        list += str(i + 1) + '. ' + animals[i] + '\n'
+    return list
+
+#Other solution(s):
